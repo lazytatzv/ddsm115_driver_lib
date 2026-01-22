427df37d16a06b9186eaa69d61dab21ecc71cd9b
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-01-23 00:37:15 +0900

    Add serialport initial configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use serialport::{DataBits, StopBits, Parity, FlowControl};
+
 fn main() {
-    println!("Hello, world!");
+    // Make this configurable later
+    let port_name = "/dev/ttyACM0";
+
+    // Based on datasheet
+    let baud_rate = 115200;
+    let data_bits = DataBits::Eight;
+    let stop_bits = StopBits::One;
+    let parity = Parity::None;
+    let flow_control = FlowControl::None;
+
+
+    let builder = serialport::new(port_name, baud_rate)
+        .stop_bits(stop_bits)
+        .data_bits(data_bits)
+        .parity(parity)
+        .flow_control(flow_control);
+
+
 }
