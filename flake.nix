34c686a36f810219a6a4a16d2b6e72329e17b747
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-01-23 03:05:02 +0900

    Update nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My Rust project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.pkg-config
+          pkgs.openssl
+        ];
+        RUST_BACKTRACE = "1";
+      };
+    };
+}
+
