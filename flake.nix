04fdcfbf2ca188682a3622f61105fa1a6196ce86
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-01-23 09:00:19 +0900

    Add systemd to flake.nix

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = [
+          pkgs.systemd
           pkgs.rustc
           pkgs.cargo
           pkgs.pkg-config
